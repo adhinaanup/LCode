652060106d4744ac1832285722d73b6330f88796
Adhina Anup <148629512+adhinaanup@users.noreply.github.com>
2025-07-05 11:00:36 +0530

    Create gcd.py

---
+++ new/gcd.py
@@ -0,0 +1,14 @@
+import math
+class Solution(object):
+    def gcd(self,a,b):
+        while b:
+            a,b=b,a%b
+        return a
+    def gcdOfStrings(self,str1,str2):
+        if str1+str2!=str2+str1:
+            return ""
+        else:
+            gcd_len=self.gcd(len(str1),len(str2))
+        return str1[:gcd_len]
+
+        
