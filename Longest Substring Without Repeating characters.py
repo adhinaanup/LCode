3bac7b7f469c38e90b097185f429e50a19979d81
Adhina Anup <148629512+adhinaanup@users.noreply.github.com>
2025-08-05 23:21:34 +0530

    Update Longest Substring Without Repeating characters.py

--- Longest Substring Without Repeating characters.py
+++ Longest Substring Without Repeating characters.py
@@ -1,1 +1,12 @@
+def longest(s):
+  longest=0
+  current=""
+  for ch in s:
+    if ch in current:
+      index=current.index(ch)
+      current=current[index+1:]
+    current+=ch
+    longest=max(longest,len(current))
+  print("Longest Substring : ",longest)
+longest("pwwkew")
 
